677063a7f4901473988b2d85df52b0eb0bf73044
Лапихин Александр <94218678+Al-Lapik@users.noreply.github.com>
2022-03-08 19:43:28 +0300

    B178 Лапихин Александр

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,21 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int low = 0, high = size - 1, count = 0, mid;
+  while (low <= high) {
+    mid = (low + high) / 2;
+    if (*(arr + mid) == value) {
+      count++;
+      break;
+    } else if (*(arr + mid) < value)
+      low = mid + 1;
+    else high = mid - 1;
+  }
+  if (count != 0) {
+    for (int i = mid + 1; *(arr + i) == value; i++)
+      count++;
+    for (int i = mid - 1; *(arr + i) == value; i--)
+      count++;
+  }
+  return count;
 }
